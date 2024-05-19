3ed9df971e23fdc04fd34690693116f5ded1f78f
David Amorim <davidfilipeamorim@gmail.com>
2024-05-19 16:31:16 +0100

    Fix OverallScore UDF

--- SQL/02_sp_functions.sql
+++ SQL/02_sp_functions.sql
@@ -3,7 +3,7 @@ USE MovieAdvisor;
 /*
     Calcula o overall score de um filme de 0 a 10 com base nas críticas do utilizador, usando os likes como ponderação.
     Exemplo: Se um filme tiver 1 review com 10 de score e 5 likes, mas tiver 2 reviews com 1 score e 0 likes, a relevância do filme será
-             Overall score = (5 * 10 + 1 + 1) / 7
+             Overall score = ((5+1) * 10 + 1 + 1) / 7
 */
 GO
 CREATE FUNCTION overallScore() RETURNS @table TABLE
@@ -47,7 +47,7 @@ BEGIN
             SELECT @LikeCount=COUNT(IsLike) FROM ReviewLikes WHERE ReviewID = @ReviewID;
 
             SET @ReviewCount = @ReviewCount + @LikeCount;
-            SET @OverallScore = @OverallScore + @LikeCount * @Classification;
+            SET @OverallScore = @OverallScore + (@LikeCount + 1) * @Classification;
 
             FETCH ReviewCursor INTO @ReviewID, @Classification;
         END
