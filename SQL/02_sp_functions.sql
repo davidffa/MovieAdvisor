e3683d0703736f1e56ea9644f3ced6b696fa7d37
David Amorim <davidfilipeamorim@gmail.com>
2024-05-21 20:02:15 +0100

    Add Views, more SPs & UDFs

--- SQL/02_sp_functions.sql
+++ SQL/02_sp_functions.sql
@@ -1,5 +1,157 @@
 USE MovieAdvisor;
 
+--------------- Views -----------------
+GO
+CREATE VIEW AllMovies
+AS SELECT AudioVisualContent.*, Runtime FROM Movie JOIN AudioVisualContent ON Movie.ID = AudioVisualContent.ID;
+
+GO
+CREATE VIEW AllSeries
+AS SELECT AudioVisualContent.*, FinishDate, State FROM TVSeries JOIN AudioVisualContent ON TVSeries.ID = AudioVisualContent.ID;
+
+GO
+CREATE VIEW GetPublicWatchlists
+AS SELECT * FROM Watchlist WHERE Visibility = 1;
+
+
+--------------- SPs -------------------
+GO
+CREATE PROC CreateEpisode(
+    @SerieID INT,
+    @SeasonNumber INT,
+    @EpNumber INT,
+	@EpRuntime SMALLINT,
+	@EpSynopsis	VARCHAR(255)
+)
+AS
+    INSERT INTO Episode (Series_ID, Season_ID, Number, Runtime, Synopsis) VALUES
+        (@SerieID, @SeasonNumber, @EpNumber, @EpRuntime, @EpSynopsis);
+
+GO
+CREATE PROC CreateSeason(
+    @SerieID INT,
+    @SeasonNumber INT,
+	@SeasonPhoto VARCHAR(128),
+	@SeasonTrailerURL VARCHAR(128),
+	@SeasonReleaseDate DATE,
+
+    @EpNumber INT,
+	@EpRuntime SMALLINT,
+	@EpSynopsis	VARCHAR(255)
+)
+AS
+    BEGIN TRAN;
+
+    INSERT INTO Season (ID, Number, Photo, TrailerURL, ReleaseDate) VALUES
+        (@SerieID, @SeasonNumber, @SeasonPhoto, @SeasonTrailerURL, @SeasonReleaseDate);
+
+    EXEC CreateEpisode @SerieID, @SeasonNumber, @EpNumber, @EpRuntime, @EpSynopsis;
+
+    COMMIT TRAN;
+
+GO
+CREATE PROC CreateSerie(
+	@Title VARCHAR(64),
+	@Synopsis VARCHAR(255),
+	@TrailerURL VARCHAR(128),
+	@Budget	MONEY,
+	@Revenue MONEY,
+	@Photo VARCHAR(128),
+	@AgeRate SMALLINT,
+	@ReleaseDate DATE,
+
+	@State VARCHAR(16),
+	@FinishDate DATE,
+
+    @SeasonNumber INT,
+	@SeasonPhoto VARCHAR(128),
+	@SeasonTrailerURL VARCHAR(128),
+	@SeasonReleaseDate DATE,
+
+    @EpNumber INT,
+	@EpRuntime SMALLINT,
+	@EpSynopsis	VARCHAR(255)
+)
+AS
+    BEGIN TRAN;
+
+    INSERT INTO AudioVisualContent (Title, Synopsis, TrailerURL, Budget, Revenue, Photo, AgeRate, ReleaseDate) VALUES
+        (@Title, @Synopsis, @TrailerURL, @Budget, @Revenue, @Photo, @AgeRate, @ReleaseDate);
+
+    DECLARE @ID INT;
+
+    SET @ID = @@IDENTITY;
+
+    INSERT INTO TVSeries (ID, State, FinishDate) VALUES
+        (@ID, @State, @FinishDate);
+
+    EXEC CreateSeason @ID, @SeasonNumber, @SeasonPhoto, @SeasonTrailerURL, @SeasonReleaseDate, @EpNumber, @EpRuntime, @EpSynopsis;
+
+    COMMIT TRAN;
+GO
+
+CREATE PROC CreateMovie(
+	@Title VARCHAR(64),
+	@Synopsis VARCHAR(255),
+	@TrailerURL VARCHAR(128),
+	@Budget	MONEY,
+	@Revenue MONEY,
+	@Photo VARCHAR(128),
+	@AgeRate SMALLINT,
+	@ReleaseDate DATE,
+
+	@Runtime INT
+)
+AS
+    BEGIN TRAN;
+
+    INSERT INTO AudioVisualContent (Title, Synopsis, TrailerURL, Budget, Revenue, Photo, AgeRate, ReleaseDate) VALUES
+        (@Title, @Synopsis, @TrailerURL, @Budget, @Revenue, @Photo, @AgeRate, @ReleaseDate);
+
+    DECLARE @ID INT;
+
+    SET @ID = @@IDENTITY;
+
+    INSERT INTO Movie (ID, Runtime) VALUES
+        (@ID, @Runtime);
+
+    COMMIT TRAN;
+GO
+
+CREATE PROC AddReview(
+	@UserID INT,
+	@AVIdentifier INT,
+	@Title VARCHAR(32),
+	@Description VARCHAR(512),
+	@Classification SMALLINT
+)
+AS
+    INSERT INTO Review(UserID, AVIdentifier, Title, Description, Classification) VALUES
+        (@UserID, @AVIdentifier, @Title, @Description, @Classification);
+GO
+
+CREATE PROC CreateWatchlist (
+    @Title VARCHAR(32),
+	@UserID INT,
+	@Visibility BIT
+)
+AS
+    INSERT INTO Watchlist (Title, UserID, Visibility) VALUES 
+        (@Title, @UserID, @Visibility)    
+GO
+
+CREATE PROC AddAVContentToWatchlist (
+    @WLTitle VARCHAR(32),
+	@UserID INT,
+	@AVIdentifier INT
+)
+AS
+    INSERT INTO WatchlistAV (WLTitle, UserID, AVIdentifier) VALUES
+        (@WLTitle, @UserID, @AVIdentifier);
+GO
+
+--------------- UDFs ----------------
+
 /*
     Calcula o overall score de um filme de 0 a 10 com base nas críticas do utilizador, usando os likes como ponderação.
     Exemplo: Se um filme tiver 1 review com 10 de score e 5 likes, mas tiver 2 reviews com 1 score e 0 likes, a relevância do filme será
@@ -57,76 +209,21 @@ AS
 RETURN (SELECT * FROM AudioVisualContent JOIN AVContentGenre ON ID=AVIdentifier WHERE GenreID = @GenreId);
 GO
 
-CREATE PROC CreateSerie(
-	@Title VARCHAR(64),
-	@Synopsis VARCHAR(255),
-	@Popularity DECIMAL(3, 1),
-	@TrailerURL VARCHAR(128),
-	@Budget	MONEY,
-	@Revenue MONEY,
-	@Photo VARCHAR(128),
-	@AgeRate SMALLINT,
-	@ReleaseDate DATE,
-
-	@State VARCHAR(16),
-	@FinishDate DATE,
-
-    @SeasonNumber INT,
-	@SeasonPhoto VARCHAR(128),
-	@SeasonTrailerURL VARCHAR(128),
-	@SeasonReleaseDate DATE,
-
-    @EpNumber INT,
-	@EpRuntime SMALLINT,
-	@EpSynopsis	VARCHAR(255)
-)
+CREATE FUNCTION getAllSeasonsOfSerie(@ID INT) RETURNS TABLE
 AS
-    BEGIN TRAN;
-
-    INSERT INTO AudioVisualContent (Title, Synopsis, Popularity, TrailerURL, Budget, Revenue, Photo, AgeRate, ReleaseDate) VALUES
-        (@Title, @Synopsis, @Popularity, @TrailerURL, @Budget, @Revenue, @Photo, @AgeRate, @ReleaseDate);
-
-    DECLARE @ID INT;
-
-    SET @ID = @@IDENTITY;
-
-    INSERT INTO TVSeries (ID, State, FinishDate) VALUES
-        (@ID, @State, @FinishDate);
-
-    INSERT INTO Season (ID, Number, Photo, TrailerURL, ReleaseDate) VALUES
-        (@ID, @SeasonNumber, @SeasonPhoto, @SeasonTrailerURL, @SeasonReleaseDate);
-
-    INSERT INTO Episode (Series_ID, Season_ID, Number, Runtime, Synopsis) VALUES
-        (@ID, @SeasonNumber, @EpNumber, @EpRuntime, @EpSynopsis);
-
-    COMMIT TRAN;
+RETURN (SELECT * FROM Season WHERE Season.ID = @ID);
 GO
 
-CREATE PROC CreateMovie(
-	@Title VARCHAR(64),
-	@Synopsis VARCHAR(255),
-	@Popularity DECIMAL(3, 1),
-	@TrailerURL VARCHAR(128),
-	@Budget	MONEY,
-	@Revenue MONEY,
-	@Photo VARCHAR(128),
-	@AgeRate SMALLINT,
-	@ReleaseDate DATE,
-
-	@Runtime INT
-)
+CREATE FUNCTION getAllEpisodesOfSeason(@SerieID INT, @SeasonNo INT) RETURNS TABLE
 AS
-    BEGIN TRAN;
+RETURN (SELECT * FROM Episode WHERE Series_ID=@SerieID AND Season_ID=@SeasonNo);
+GO
 
-    INSERT INTO AudioVisualContent (Title, Synopsis, Popularity, TrailerURL, Budget, Revenue, Photo, AgeRate, ReleaseDate) VALUES
-        (@Title, @Synopsis, @Popularity, @TrailerURL, @Budget, @Revenue, @Photo, @AgeRate, @ReleaseDate);
-
-    DECLARE @ID INT;
-
-    SET @ID = @@IDENTITY;
-
-    INSERT INTO Movie (ID, Runtime) VALUES
-        (@ID, @Runtime);
-
-    COMMIT TRAN;
+CREATE FUNCTION getAVContentReviews(@ID INT) RETURNS TABLE
+AS
+RETURN (
+    SELECT R.*, LikeCount=COUNT(IsLike) 
+    FROM (SELECT * FROM Review WHERE AVIdentifier=@ID) AS R JOIN ReviewLikes ON R.ID = ReviewLikes.ReviewID 
+    GROUP BY R.ID, R.UserID, R.AVIdentifier, R.Title, R.Description, R.Classification, R.CreatedAt
+);
 GO
