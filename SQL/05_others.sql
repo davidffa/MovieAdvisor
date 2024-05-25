1d807950958d8ba4771c8acda45e5d4ce27652a1
David Amorim <davidfilipeamorim@gmail.com>
2024-05-25 22:00:58 +0100

    Add more indexes

--- SQL/05_others.sql
+++ SQL/05_others.sql
@@ -1,7 +1,17 @@
 USE MovieAdvisor;
 
-CREATE INDEX IX_AVTitle ON AudioVisualContent(Title)
+-- Search content by title
+CREATE NONCLUSTERED INDEX IX_AVTitle ON AudioVisualContent(Title)
 GO
 
-CREATE INDEX IX_PersonName ON Person(Name);
+-- Sort by release date
+CREATE NONCLUSTERED INDEX IX_ReleaseDate ON AudioVisualContent(ReleaseDate)
+GO
+
+-- Search by person name
+CREATE NONCLUSTERED INDEX IX_PersonName ON Person(Name);
+GO
+
+-- User lookup is made by email
+CREATE NONCLUSTERED INDEX IX_UserEmail ON "User"(Email);
 GO
